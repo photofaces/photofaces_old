4ce49a0a7b18b57a51e342a2769c72971ac2baf3
Marcus Campos <55887372+MarcusCampos1997@users.noreply.github.com>
2020-01-25 21:15:26 -0300

    Add files via upload
    Começando a calcular resultados com o silhouette_score

--- cluster_faces.py
+++ cluster_faces.py
@@ -3,6 +3,7 @@
 
 # import the necessary packages
 from sklearn.cluster import DBSCAN, KMeans, AffinityPropagation, MeanShift, SpectralClustering, AgglomerativeClustering, OPTICS,Birch
+from sklearn import metrics
 from sklearn.metrics import accuracy_score
 from imutils import build_montages
 import numpy as np
@@ -12,111 +13,88 @@ import cv2
 import time #medir tempo
 import os
 import shutil
+from sklearn.datasets import make_blobs
+
+from sklearn.preprocessing import StandardScaler
 
 
-# construct the argument parser and parse the arguments
-def teste():
-	print('testando')
-
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-e", "--encodings", required=True,
-#	help="path to serialized db of facial encodings")
-#ap.add_argument("-j", "--jobs", type=int, default=-1,
-#	help="# of parallel jobs to run (-1 will use all CPUs)")
-#args = vars(ap.parse_args())
 
 def cluster(endereco_enc,origem_fotos, destino_fotos):
-    # load the serialized face encodings + bounding box locations from
-    # disk, then extract the set of encodings to so we can cluster on
-    # them
-    #print("AAAA")    
-    #print(destino_fotos)     
-    print("[INFO] loading encodings...")
-    #pickle.loads(open(args["encodings"], "rb").read())  
-    data = pickle.loads(open(endereco_enc, "rb").read())
-    data = np.array(data)
-    #print("This is the data")    
-    #print(data)       
-    encodings = [d["encoding"] for d in data]
-    #print(encodings)
-    data = np.array(data)
-    #print("This is the encoding")        
-    # cluster the embeddings
-    print("[INFO] clustering...")
 
-    inicio = time.time()
+    print("[INFO] loading encodings...")
+ 
+ 
+    #carrega os encodings do disco
+    data = pickle.loads(open(endereco_enc, "rb").read())
+    
+    #os coloca em um vetor numpy
+    data = np.array(data)
+    
+    #extrai os encodings, colocando-os em uma lista
+    encodings = [d["encoding"] for d in data]
+
+    print("[INFO] clustering...")
+    
+    #inicio = time.time()
 
     #num correto de clusters = 8
-    clt = KMeans(n_clusters=5 , init='k-means++', n_init=10, max_iter=300, tol=0.0001, precompute_distances='auto', verbose=0, random_state=None, copy_x=True, n_jobs=None, algorithm='auto')
+    #clt = KMeans(n_clusters=6 , init='k-means++', n_init=10, max_iter=300, tol=0.0001, precompute_distances='auto', verbose=0, random_state=None, copy_x=True, n_jobs=None, algorithm='auto')
     #print(clt)
 
-
-    #clt = AffinityPropagation(damping=0.5, max_iter=200, convergence_iter=15, copy=True, preference=None, affinity='euclidean', verbose=False)
-
+    
+    #X, Y = make_blobs()
+    
     # retirou um parâmetro...
-    #clt = MeanShift(bandwidth=0.5, seeds=None, bin_seeding=False, min_bin_freq=1, cluster_all=True, n_jobs=None)
+    #clt = MeanShift(bandwidth=0.5, seeds=None, bin_seeding=False, min_bin_freq=1, cluster_all=True, n_jobs=None).fit(X)
 
-    # retirou o parâmetro n_components = none
-    #clt =SpectralClustering(n_clusters=8, eigen_solver=None, random_state=None, n_init=10, gamma=1.0, affinity='rbf', n_neighbors=10, eigen_tol=0.0, assign_labels='kmeans', degree=3, coef0=1, kernel_params=None, n_jobs=None)
-
-
-    #clt = AgglomerativeClustering(n_clusters=10, affinity='euclidean', memory=None, connectivity=None, compute_full_tree='auto', linkage='ward', distance_threshold=None)
-
-    #clt = DBSCAN(metric="euclidean", n_jobs= -1)
-
-    #print(type(clt))
-
-    #retirou parâmetro max_eps=inf, min_samples = 2 foi um bom resultado
-
-    #clt = OPTICS(min_samples=2, max_eps = 99999999999, metric='minkowski', p=2, metric_params=None, cluster_method='xi', eps=None, xi=0.05, predecessor_correction=True, min_cluster_size=None, algorithm='auto', leaf_size=30, n_jobs=None)
-
-    # ??
-    #clt = Birch(threshold=0.2, branching_factor=50, n_clusters=3, compute_labels=True, copy=True)
+    
+    #print("AAAAA")
+    #print(labels)
+    
+    clt = DBSCAN(metric='euclidean')
     
     #Aqui que realmente acontece a clusterização
-    clt.fit(encodings)
 
-    fim = time.time()
+    
+    l = clt.fit(encodings)
+    laabels = l.labels_
 
-    #depois de arrumar as funcionalidades, VOLTAR
+    n =  metrics.silhouette_score(encodings, laabels)
+
+    print(n)
+
+    #fim = time.time()
+    
     #print("O tempo de execução foi:" + str(fim - inicio))
-
-
-    # y_true é um array com os verdadeiros, y_pred, é o resultado
-    #accuracy = accuracy_score(y_true, y_pred, normalize=True, sample_weight=None)
-
 
     # determine the total number of unique faces found in the dataset
     labelIDs = np.unique(clt.labels_)
-    #print(labelIDs)    
+    
+
     #print(clt.labels_)# determine the total number of unique faces found in the dataset    
     numUniqueFaces = len(np.where(labelIDs > -1)[0])
     print("[INFO] # unique faces: {}".format(numUniqueFaces))
 
+
     
-    #print( "esta é a lista GGGGGGGGGGGGGGGGGGGGGGGGG:" )
-    #endereco_fotos_dataset = Identifica_fotos(origem_fotos, destino_fotos)     
-    
-    #print(endereco_fotos_dataset)
-    
-    #print(g)
-    #print(type(g))
     # loop over the unique face integers
     for labelID in labelIDs:
         # find all indexes into the `data` array that belong to the
         # current label ID, then randomly sample a maximum of 25 indexes
         # from the set
         print("[INFO] faces for face ID: {}".format(labelID))
-        #print("AAA" +str(labelID))
+        
+        
+
+
+
         idxs = np.where(clt.labels_ == labelID)[0]
-        #print("clt label:"+ str(clt.labels_))
-        #print("idx: " +str(idxs))        
+       
         idxs = np.random.choice(idxs, size=min(25, len(idxs)),
             replace=False)
 
         # initialize the list of faces to include in the montage
         faces = []
-        
         
         
         #endereco_pasta = os.path.join(data[i]["imagePath"], destino_fotos, str(labelIDS))
@@ -125,19 +103,16 @@ def cluster(endereco_enc,origem_fotos, destino_fotos):
         
         Cria_Pastas(endereco_pasta)
 
-    
-            
+
+        
 
         # loop over the sampled indexes
         for i in idxs:
+        
+
             # load the input image and extract the face ROI
             image = cv2.imread(data[i]["imagePath"])
-
-            # ALASSSS I FOUND YA MUAHAHAHAHAHAHAHAHA
-            #print(data[i]["imagePath"])            
             
-           
-           
             #então, coloca as fotos lá dentro
             Move_Fotos(data[i]["imagePath"],endereco_pasta)
 
@@ -149,18 +124,23 @@ def cluster(endereco_enc,origem_fotos, destino_fotos):
             # faces montage list
             face = cv2.resize(face, (96, 96))
             faces.append(face)
+                    
+                    
+                    #testando os algoritmos acuracia
+
+
+           # print("o número de clusters é: " + str(n_clusters_))           
+            
 
         # create a montage using 96x96 "tiles" with 5 rows and 5 columns
         
-        
-
         montage = build_montages(faces, (96, 96), (5, 5))[0]
         
         # show the output montage
-        #title = "Face ID #{}".format(labelID)
-        #title = "Unknown Faces" if labelID == -1 else title
-        #cv2.imshow(title, montage)
-        #cv2.waitKey(0)
+        title = "Face ID #{}".format(labelID)
+        title = "Unknown Faces" if labelID == -1 else title
+        cv2.imshow(title, montage)
+        cv2.waitKey(0)
  
  
  #origem
@@ -176,8 +156,9 @@ def Cria_Pastas(endereco):
         if not os.path.exists(endereco):
             os.mkdir(endereco)
    
-    
-        
+
+
+
         
 
  
